9c8e25fa0fb977656501e439dc30e9b2d397223f
Sand <sandzaa123@gmail.com>
2026-03-20 10:48:21 +0700

    feat: add CandidateRegistrationView component

--- components/CandidateRegistrationView.tsx
+++ components/CandidateRegistrationView.tsx
@@ -193,7 +193,7 @@ export default function CandidateRegistrationView({ initialEmployee, userImageUr
                             ) : (
                               <>
                                 <UserPlus className="w-4 h-4 mr-2" />
-                                สมัครการชุดนี้
+                                ลงสมัครการเลือกตั้ง
                               </>
                             )}
                           </Button>
